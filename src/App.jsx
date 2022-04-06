d0c60de4eb7cb68576da00d9ec8f2757c242c404
Beatriz Dominguez Diaz <isc18350304@gmail.com>
2022-04-06 09:20:04 -0500

    todo lo de ayer

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,27 @@ import {useState} from "react";
 function App (){
 
   const [cuenta, setCuenta] = useState(0);
+  const [paso, setPaso] = useState(1);
 
-  const handleClick = () => {
-    setCuenta(cuenta + 1);
+  const handleMinusClick = () => {
+    setCuenta(cuenta - paso);
+  };
+
+  const handleResetClick = () => {
+    setCuenta(0);
+    setPaso(1);
+  };
+
+  const handlePlusClick = () => {
+    setCuenta(cuenta + paso);
+  };
+
+  const handleInputChange = (e) => {
+
+    if (isNaN(e.target.value)){
+      return;
+    }
+    setPaso(Number(e.target.value));
   };
 
   return (
@@ -19,11 +37,43 @@ function App (){
         justifyContent:"flex-end",
         marginRight: "5px",
         }}> 
+
+        <label>
+          Paso
+          <input 
+          id="paso"
+          type= "text" 
+          onChange = {handleInputChange}
+          value={paso}
+          style= {{marginLeft: "5px", width: "60 px"}}
+          />
+        </label>
         
         <button 
           type = "button" 
           className ="btn btn-primary"
-          onClick={handleClick}
+          onClick={handleMinusClick}
+          style= {{ marginLeft: "5px"}}
+          >
+            -1
+            
+          </button>
+
+        <button 
+          type = "button" 
+          className ="btn btn-primary"
+          onClick={handleResetClick}
+          style= {{marginLeft: "5px"}}
+          >
+            RESET
+            
+          </button>
+
+        <button 
+          type = "button" 
+          className ="btn btn-primary"
+          onClick={handlePlusClick}
+          style= {{marginLeft: "5px"}}
           >+1</button>
       </div>
     </div>
